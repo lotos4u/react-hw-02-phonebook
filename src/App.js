8d9d3ee89ce997c848904959fed0e97eeeb1b8b7
Denis Golovin <golovin.denis@pandats.com>
2020-08-26 15:24:09 +0300

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, {Component} from 'react';
 import './App.css';
 import ContactForm from "./components/ContactForm";
 import ContactsList from "./components/ContactsList";
+import FilterForm from "./components/FilterForm";
 
 class App extends Component {
     state = {
-        contacts: [],
-        name: '',
-        number: '',
+        contacts: [
+            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        ],
+        filter: '',
     };
 
     handleSubmit = (contact) => {
@@ -19,11 +24,27 @@ class App extends Component {
         });
     };
 
+    getFilteredContacts() {
+        if (!this.state.filter) {
+            return this.state.contacts;
+        }
+        return this.state.contacts.filter(c => c.name.toLowerCase().includes(this.state.filter.toLowerCase()));
+    }
+
+    handleFilter(filter) {
+        this.setState({
+            ...this.state,
+            filter: filter,
+        });
+    }
+
     render() {
         return <>
             <h1>Phonebook</h1>
             <ContactForm onAdd={this.handleSubmit}/>
-            <ContactsList contacts={this.state.contacts}/>
+            <h1>Contacts</h1>
+            <FilterForm onFilter={(f) => this.handleFilter(f)}/>
+            <ContactsList contacts={this.getFilteredContacts()}/>
         </>;
     }
 }
--- src/components/ContactsList/index.jsx
+++ src/components/ContactsList/index.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 import ContactsItem from "../ContactItem";
-import FilterForm from "../FilterForm";
 
 class ContactsList extends Component {
 
@@ -9,27 +8,12 @@ class ContactsList extends Component {
         filter: '',
     };
 
-    handleFilter(filter) {
-        this.setState({
-            filter: filter,
-        });
-    }
-
-    getFilteredContacts() {
-        if (!this.state.filter) {
-            return this.props.contacts;
-        }
-        return this.props.contacts.filter(c => c.name.includes(this.state.filter));
-    }
-
     render() {
         return <>
             {this.props.contacts.length > 0 &&
             <>
-                <h1>Contacts</h1>
-                <FilterForm onFilter={(f) => this.handleFilter(f)}/>
                 <ul>
-                    {this.getFilteredContacts().map(item => <ContactsItem key={item.id} name={item.name} number={item.number}/>)}
+                    {this.props.contacts.map(item => <ContactsItem key={item.id} name={item.name} number={item.number}/>)}
                 </ul>
             </>
             }
