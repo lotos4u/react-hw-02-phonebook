aba6264668c6ed5c3359b2edb8fbe7172624359b
Denis Golovin <golovin.denis@pandats.com>
2020-08-26 15:16:33 +0300

    Updates

---
+++ precache-manifest.dedbc17c9b459403ce3eb7330468c9c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c06c7e82c8979c50866157e716711e",
+    "url": "/react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "1a786d9cc0f1cfb2008e",
+    "url": "/react-hw-02-phonebook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56dffbab77761e25be2c",
+    "url": "/react-hw-02-phonebook/static/js/2.6e95c0de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-02-phonebook/static/js/2.6e95c0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a786d9cc0f1cfb2008e",
+    "url": "/react-hw-02-phonebook/static/js/main.aead2a7e.chunk.js"
+  },
+  {
+    "revision": "6f482f3e5b3d66793c08",
+    "url": "/react-hw-02-phonebook/static/js/runtime-main.2c5ed82f.js"
+  }
+]);
